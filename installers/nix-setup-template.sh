fa60770c5defd28dc0a9a621d1a624238a5fc406
Zach Vickery <zach.vickery@sifthealthcare.com>
2025-09-24 19:16:04 -0500

    testing fix

--- installers/nix-setup-template.sh
+++ installers/nix-setup-template.sh
@@ -51,7 +51,6 @@ chmod +x ../python $PYTHON_MAJOR $PYTHON_MAJOR_DOT_MINOR $PYTHON_MAJORMINOR pyth
 
 echo "Upgrading pip..."
 export PIP_ROOT_USER_ACTION=ignore
-./python -m ensurepip
 ./python -m pip install --upgrade --force-reinstall pip --disable-pip-version-check --no-warn-script-location
 
 echo "Create complete file"
